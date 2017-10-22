aa948e4f8619cf581fa01dd51a921555b518ece4
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-22 18:28:27 +0200

    Add files via upload

---
+++ AA_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czy NumLock jest włączony? {0}", Console.NumberLock);
+            Console.ReadKey();
+        }
+    }
+}
